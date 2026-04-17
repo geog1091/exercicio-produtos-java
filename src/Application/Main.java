4dfe9037b0eaa1b4813a9659cca8a3831baea765
Geovanni Guimarães <geovanniog.work@gmail.com>
2026-04-16 21:47:38 -0300

    Update Main.java

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); 
 		
-		Product produto = new Product();
+		Product produto = new Product(); //criação de uma variável da classe Product
 		System.out.println("Bem-vindo ao sistema de estoque.");
 		System.out.println("Insira o nome do produto:");
 		produto.nome = sc.nextLine();
@@ -22,14 +22,14 @@ public class Main {
 		
 		System.out.printf("\n\n Insira a quantidade que será adicionada ao estoque:\n");
 		
-		int quant = sc.nextInt();
+		int quant = sc.nextInt(); // usuário insere o input de quanto será adicionado ao estoque
 		produto.adicionarProdutos(quant);
 		
 		System.out.println("Informações do produto (ATUALIZADO!!!): " + produto);
 		
 		System.out.printf("\n\n Insira a quantidade que será removida do estoque\nS");
 		
-		quant = sc.nextInt();
+		quant = sc.nextInt(); // usuário insere o input de quanto será removido do estoque
 		produto.removerProdutos(quant);
 		
 		System.out.println("Informações do produto (ATUALIZADO!!! FINAL!!!): " + produto);
